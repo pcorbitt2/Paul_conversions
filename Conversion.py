85f5abd92bd79c9be4497096a2e8d1fa889cf7aa
Amrita Roy Choudhury <choudhury.amritaroy@gmail.com>
2016-10-14 10:27:15 -0400

    Implemented Hours to Minutes

--- Conversion.py
+++ Conversion.py
@@ -9,6 +9,10 @@ def dollar2cents(dollars):
 #Converting Gallong to Liters
 
 def gallons2liters(gallons):
-	liters = gallons/ 3.78
+	liters = gallons / 3.78
 	return(liters)
 	
+#Converting Hours to Minutes
+def hours2minutes(hours):
+	minutes = hours * 60
+	return minutes
