2268a66521bb717ee06009592bad69263b869547
Amrita Roy Choudhury <choudhury.amritaroy@gmail.com>
2016-10-14 09:55:58 -0400

    Implemented Gallons to Liters conversion

--- Conversion.py
+++ Conversion.py
@@ -1,7 +1,14 @@
 # My conversion tools
 
 #Converting Dollars to cents
-def dollar2cents(dollars)
+
+def dollar2cents(dollars):
 	cents = dollars * 100
 	return(cents)
+	
+#Converting Gallong to Liters
+
+def gallons2liters(gallons):
+	liters = gallons * 3.78
+	return(liters)
 	
